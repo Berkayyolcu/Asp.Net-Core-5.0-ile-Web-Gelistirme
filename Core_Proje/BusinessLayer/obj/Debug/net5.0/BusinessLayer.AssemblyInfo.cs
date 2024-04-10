32f473cacfcc0e5479ab4c356927bfe784460241
Berkay Yolcu <berkayyolcu99@gmail.com>
2024-04-10 03:06:18 +0300

    deneme

--- Core_Proje/BusinessLayer/obj/Debug/net5.0/BusinessLayer.AssemblyInfo.cs
+++ Core_Proje/BusinessLayer/obj/Debug/net5.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8add2cf3b1c0e561ebd280407d623835d8d2acfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Core_Proje/Core_Proje/obj/Debug/net5.0/Core_Proje.RazorTargetAssemblyInfo.cs
+++ Core_Proje/Core_Proje/obj/Debug/net5.0/Core_Proje.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core_Proje")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8add2cf3b1c0e561ebd280407d623835d8d2acfe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core_Proje.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
